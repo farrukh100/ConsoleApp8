09f81f44e9f0a7dbcd9bd8d6cf71a10f82eff44a
farrukh100 <farrukhcontour@gmail.com>
2019-11-25 15:04:31 +0500

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Test message");
+            Console.ReadLine();
+        }
+    }
+}
